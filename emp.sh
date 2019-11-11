ccaaa10fe4df0bce0e2140a162f9403b3c2a5476
mugiwarapirates <hemilturakhia@gmail.com>
2019-11-11 14:13:37 +0530

    Functional Data

--- emp.sh
+++ emp.sh
@@ -2,17 +2,29 @@
 
 partTime=1
 fullTime=2
-TimePerHours=20
-empRandomCheck=$((RANDOM%3))
-case $empRandomCheck in 
-$partTime)
-   emphour=4;;
+maxHoursInMonth=50
+EmpRatePerHour=20
+numberOfWorkingDays=20
+totalEmphours=0
+totalWorkingDays=0
+function  getWorkingDays()
+{
+	case $1 in 
+	$partTime)
+   		emphour=4;;
 
-$fullTime)
-  emphour=8;;
-0)
-emphour=0;;
-esac
-salary=$(($TimePerHours * $emphour))
-echo $salary
-    
+	$fullTime)
+  		emphour=8;;
+	*)
+		emphour=0;;
+	esac
+	echo $emphour
+}
+
+while [[ $totalEmphours -lt $maxHoursInMonth &&  $totalEmphours -lt $numberOfWorkingDays ]]
+do
+  ((totalEmphours++))
+	emphour="$( getWorkingDays $((RANDOM%3)) )"                                           
+    	totalWorkingHour=$(($totalEmphours+$emphour))
+done
+    totalSalary=$(($totalWorkingHour*$EmpRatePerHour))
